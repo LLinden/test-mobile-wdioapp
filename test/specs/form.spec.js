e38354b20b0acc5b5b5a73801da747cc48b1aa34
Lucas Linden <lucas.linden@cwi.com.br>
2022-11-11 10:38:20 -0300

    first commit

---
+++ test/specs/form.spec.js
@@ -0,0 +1,8 @@
+
+
+describe('Access App and Fill Form', () => {
+    it('should successfully fill form', async () => {
+       
+        
+    });
+});
---
+++ wdio.conf.js
@@ -0,0 +1,20 @@
+const {join} = require('path')
+
+exports.config = {
+    hostname: 'localhost',
+    port: 4723,
+    path: '/wd/hub',
+    specs: [
+        './test/specs/**/*.spec.js'
+    ],
+    framework: 'mocha',
+    capabilities: [{
+        "platformName": "Android",
+        "platformVersion": "9.0",
+        "deviceName": "Pixel 2 API 28",
+        "automationName": "UiAutomator2",
+        "app": join(process.cwd(), './app/android/Android-NativeDemoApp-0.4.0.apk'),
+        "adbExecTimeout": 40000,
+        "appWaitActivity": "com.wdiodemoapp/.MainActivity"
+    }]
+}
